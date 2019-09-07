41d17ce769b7a69d2cf2523bb7af7f4e728f5db6
Masanori SUTO <suto.secondary@gmail.com>
2019-09-07 17:01:18 +0900

    Exclude nginx and php

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,20 @@
-FROM ubuntu:16.04
+FROM alpine:3.10
 
 MAINTAINER Andrey Mamaev <asda@asda.ru>
 
-ENV MYSQLTMPROOT toor
+RUN apk --update add \
+	mariadb-client \
+	freeradius \
+	freeradius-mysql \
+    rm -rf /var/cache/apk/*
 
-RUN apt-get update && apt-get install -y mysql-client libmysqlclient-dev \
-  nginx php php-common php-gd php-curl php-mail php-mail-mime php-pear php-db php-mysqlnd \
-  freeradius freeradius-mysql freeradius-utils \
-  wget unzip && \
-  pear install DB && \
-  apt-get clean && \
-  rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /root/.cpan	
+ENV RADIUS_DB_PWD=radpass
+ENV CLIENT_NET="0.0.0.0/0"
+ENV CLIENT_SECRET=testing123
 
-ENV RADIUS_DB_PWD radpass
-ENV CLIENT_NET "0.0.0.0/0"
-ENV CLIENT_SECRET testing123
-
-
-RUN wget https://github.com/lirantal/daloradius/archive/master.zip && \
-	unzip *.zip && \
-	mv daloradius-master /var/www/daloradius && \
- 	chown -R www-data:www-data /var/www/daloradius && \
-	chmod 644 /var/www/daloradius/library/daloradius.conf.php && \
-	rm /etc/nginx/sites-enabled/default
-
-#	cp -R /var/www/daloradius/contrib/chilli/portal2/hotspotlogin /var/www/daloradius
-
+RUN  ln -s /etc/freeradius/3.0/mods-available/sql /etc/freeradius/3.0/mods-enabled
 COPY init.sh /	
-COPY etc/nginx/radius.conf /etc/nginx/sites-enabled/
 
-
-
-EXPOSE 1812 1813 80
+EXPOSE 1812 1813
 
 ENTRYPOINT ["/init.sh"]
-
--- init.sh
+++ init.sh
@@ -1,72 +1,58 @@
 #!/bin/bash
 
-mysql --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD $RADIUS_DB_NAME  < /etc/freeradius/3.0/main/mysql/schema.sql
-mysql --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD $RADIUS_DB_NAME  < /etc/freeradius/3.0/main/mysql/nas.sql
-mysql --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD $RADIUS_DB_NAME  < /var/www/daloradius/contrib/db/mysql-daloradius.sql
+echo Waiting for MySQL daemon to be ready...
+until mysql --host=$RADIUS_DB_HOST --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD &> /dev/null; do
+    sleep 1
+done
+echo MySQL is up. Continuing...
+
+mysql --host=$RADIUS_DB_HOST --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD $RADIUS_DB_NAME  < /etc/freeradius/3.0/mods-config/sql/main/mysql/schema.sql
+
+sed -i 's/dialect = "sqlite"/dialect = "mysql"/' /etc/freeradius/3.0/mods-available/sql
+sed -i 's/#\s*server = localhost/server = '$RADIUS_DB_HOST'/' /etc/freeradius/3.0/mods-available/sql
+sed -i 's/#\s*port = 3306/port = 3306/' /etc/freeradius/3.0/mods-available/sql
+sed -i 's/password = "radpass"/password = "'$RADIUS_DB_PWD'"/' /etc/freeradius/3.0/mods-available/sql
+sed -i -e 's|authorize {|authorize {\nsql|' /etc/freeradius/3.0/sites-available/inner-tunnel
+sed -i -e 's|session {|session {\nsql|' /etc/freeradius/3.0/sites-available/inner-tunnel 
+sed -i -e 's|authorize {|authorize {\nsql|' /etc/freeradius/3.0/sites-available/default
+sed -i -e 's|session {|session {\nsql|' /etc/freeradius/3.0/sites-available/default
+sed -i -e 's|accounting {|accounting {\nsql|' /etc/freeradius/3.0/sites-available/default
+
+sed -i -e 's|auth_badpass = no|auth_badpass = yes|g' /etc/freeradius/3.0/radiusd.conf
+sed -i -e 's|auth_goodpass = no|auth_goodpass = yes|g' /etc/freeradius/3.0/radiusd.conf
+sed -i -e 's|auth = no|auth = yes|g' /etc/freeradius/3.0/radiusd.conf
+
+sed -i -e 's|\t#  See "Authentication Logging Queries" in sql.conf\n\t#sql|#See "Authentication Logging Queries" in sql.conf\n\tsql|g' /etc/freeradius/3.0/sites-available/inner-tunnel 
+sed -i -e 's|\t#  See "Authentication Logging Queries" in sql.conf\n\t#sql|#See "Authentication Logging Queries" in sql.conf\n\tsql|g' /etc/freeradius/3.0/sites-available/default
 
 
-sed -i 's/password = "radpass"/password = "'$RADIUS_DB_PWD'"/' /etc/freeradius/sql.conf
-sed -i 's/#port = 3306/port = 3306/' /etc/freeradius/sql.conf
-sed -i 's/#server = localhost/server = radius/' /etc/freeradius/3.0/sql.conf
-sed -i -e 's/$INCLUDE sql.conf/\n$INCLUDE sql.conf/g' /etc/freeradius/radiusd.conf
-sed -i -e 's|$INCLUDE sql/mysql/counter.conf|\n$INCLUDE sql/mysql/counter.conf|g' /etc/freeradius/radiusd.conf
-sed -i -e 's|authorize {|authorize {\nsql|' /etc/freeradius/sites-available/inner-tunnel
-sed -i -e 's|session {|session {\nsql|' /etc/freeradius/sites-available/inner-tunnel 
-sed -i -e 's|authorize {|authorize {\nsql|' /etc/freeradius/sites-available/default
-sed -i -e 's|session {|session {\nsql|' /etc/freeradius/sites-available/default
-sed -i -e 's|accounting {|accounting {\nsql|' /etc/freeradius/sites-available/default
-
-sed -i -e 's|auth_badpass = no|auth_badpass = yes|g' /etc/freeradius/radiusd.conf
-sed -i -e 's|auth_goodpass = no|auth_goodpass = yes|g' /etc/freeradius/radiusd.conf
-sed -i -e 's|auth = no|auth = yes|g' /etc/freeradius/radiusd.conf
-
-sed -i -e 's|\t#  See "Authentication Logging Queries" in sql.conf\n\t#sql|#See "Authentication Logging Queries" in sql.conf\n\tsql|g' /etc/freeradius/sites-available/inner-tunnel 
-sed -i -e 's|\t#  See "Authentication Logging Queries" in sql.conf\n\t#sql|#See "Authentication Logging Queries" in sql.conf\n\tsql|g' /etc/freeradius/sites-available/default
-
-sed -i -e 's|sqltrace = no|sqltrace = yes|g' /etc/freeradius/sql.conf
+sed -i -e "s/readclients = yes/nreadclients = yes/" /etc/freeradius/3.0/mods-available/sql
+echo -e "\nATTRIBUTE Usage-Limit 3000 string\nATTRIBUTE Rate-Limit 3001 string" >> /etc/freeradius/3.0/dictionary
 
 
 
-sed -i -e "s/readclients = yes/nreadclients = yes/" /etc/freeradius/sql.conf
-echo -e "\nATTRIBUTE Usage-Limit 3000 string\nATTRIBUTE Rate-Limit 3001 string" >> /etc/freeradius/dictionary
-
-
-
-#================DALORADIUS=========================
-sed -i "s/$configValues\['CONFIG_DB_PASS'\] = '';/$configValues\['CONFIG_DB_PASS'\] = '"$RADIUS_DB_PWD"';/" /var/www/daloradius/library/daloradius.conf.php
-sed -i "s/$configValues\['CONFIG_DB_USER'\] = 'root';/$configValues\['CONFIG_DB_USER'\] = '"$RADIUS_DB_USER"';/" /var/www/daloradius/library/daloradius.conf.php
 
 if [ -n "$CLIENT_NET" ]; then
 echo "client $CLIENT_NET {
     	secret          = $CLIENT_SECRET
     	shortname       = clients
-}" >> /etc/freeradius/clients.conf
-fi 
+}" >> /etc/freeradius/3.0/clients.conf
+fi
 
 
 #======== DELETE INIT CODE ==
 echo "#!/bin/bash
 
-echo Waiting for MySQL daemon to brought up...
-until mysql --host=radius --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD &> /dev/null; do
+echo Waiting for MySQL daemon to be ready...
+until mysql --host=mysql --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD &> /dev/null; do
     sleep 1
 done
 echo MySQL is up. Continuing...
-
-php7.2-fpm & 
-nginx & 
+nginx &
 /usr/sbin/freeradius -X" > /init.sh
 
-
+echo "debug printf"
 mkdir /run/php & \
-
-echo Waiting for MySQL daemon to brought up...
-until mysql --host=radius --user=$RADIUS_DB_USER --password=$RADIUS_DB_PWD &> /dev/null; do
-    sleep 1
-done
-echo MySQL is up. Continuing...
-
-php7.2-fpm & \
 nginx & \
 /usr/sbin/freeradius -X
 
